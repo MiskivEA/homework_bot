2602eecd86af17b1733c14a739a8663475f944ab
Евгений Миськив <evgenymiskiv@gmail.com>
2022-10-31 16:39:06 +0300

    prereview 7 pep 8

--- homework.py
+++ homework.py
@@ -53,7 +53,8 @@ def send_message(bot, message):
 
 def get_api_answer(current_timestamp):
     """Запрос к АПИ домашки.
-    Возвращает словарь с работами и текущим временем"""
+    Возвращает словарь с работами и текущим временем
+    """
     timestamp = current_timestamp
     params = {'from_date': timestamp}
     response = requests.get(ENDPOINT, headers=HEADERS, params=params)
